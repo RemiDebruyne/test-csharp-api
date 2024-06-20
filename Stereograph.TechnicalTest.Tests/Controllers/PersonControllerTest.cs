f0d65005ea740b9ca794837aa9e0452ec7a086a4
Remi Debruyne <remidebruyne@hotmail.com>
2024-06-20 07:02:03 +0200

    feat: début des tests unitaires

---
+++ Stereograph.TechnicalTest.Tests/Controllers/PersonControllerTest.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+using System.Threading.Tasks;
+using Stereograph.TechnicalTest.Api.Repositories;
+using FakeItEasy;
+using Stereograph.TechnicalTest.Api.Controllers;
+using Stereograph.TechnicalTest.Api.Entities;
+using Microsoft.AspNetCore.Mvc;
+using FluentAssertions;
+
+namespace Stereograph.TechnicalTest.Tests.Controllers
+{
+    public class PersonControllerTest
+    {
+        IRepository<Person> _repository;
+
+        public PersonControllerTest()
+        {
+            _repository = A.Fake<IRepository<Person>>();
+        }
+
+        [Fact]
+        public async Task PersonController_GetAllPerson_ReturnOKAsync()
+        {
+            //Arrange
+
+            var controller = new PersonController(_repository);
+
+
+            //Act
+            var actionResult = await controller.GetAll();
+
+            //Assert
+            actionResult.Should().BeOfType(typeof(OkObjectResult));
+        }
+
+        [Fact]
+        public async Task PersonController_GetPersonById_ReturnOKAsync()
+        {
+            //Arrange
+            var controller = new PersonController(_repository);
+            int testId = 5;
+
+            //Act
+            var actionResult = await controller.GetById(testId);
+
+            //Assert
+            actionResult.Should().BeOfType(typeof (OkObjectResult));
+        }
+
+        [Fact]
+        public async Task PersonController_Add_ReturnCreatedAtAction()
+        {
+            //Arrange
+            var person = A.Fake<Person>();
+            var controller = new PersonController(_repository);
+
+            //Act
+            var actionResult = await controller.Add(person);
+
+            //Assert
+            actionResult.Should().BeOfType(typeof(CreatedAtActionResult));
+
+        }
+
+
+    }
+}
