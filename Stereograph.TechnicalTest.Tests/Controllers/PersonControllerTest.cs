6435cf3d253035fe00d73afccc3bb17c1edf0a57
Remi Debruyne <remidebruyne@hotmail.com>
2024-06-21 11:10:15 +0200

    feat: fin de la rédaction des tests techniques J'ai modifié la libraire de mocking FakeItEasy pour Moq. J'ai rencontré un problème avec FakeItEasy qui m'empêchait de configurer correctement les appellent à mon repository. Impossible de trouver une solution et j'ai finalement décidé de tester Moq, problème résolu instanténément. J'ai également décidé de supprimer Fluent Assertion. N'étant pas habiuté aux tests techniques, j'ai préféré rester avec le fonctionnement "vanilla" pour me familiariser, ainsi qu'avoir une meilleur documentation.
    Les tests testent si les actions retournent le resultat attendu, ainsi que
    certaines valeurs retournées. J'aurais souhaité tester si l'action
    Add() (ma route HttpPut) retournait correctement actionName, routeValue et value
    mais je n'ai pas trouvé comment faire.

--- Stereograph.TechnicalTest.Tests/Controllers/PersonControllerTest.cs
+++ Stereograph.TechnicalTest.Tests/Controllers/PersonControllerTest.cs
@@ -5,67 +5,264 @@ using System.Text;
 using Xunit;
 using System.Threading.Tasks;
 using Stereograph.TechnicalTest.Api.Repositories;
-using FakeItEasy;
 using Stereograph.TechnicalTest.Api.Controllers;
 using Stereograph.TechnicalTest.Api.Entities;
 using Microsoft.AspNetCore.Mvc;
-using FluentAssertions;
+using System.Linq.Expressions;
+using Moq;
+using Microsoft.VisualStudio.TestPlatform.ObjectModel.DataCollection;
 
 namespace Stereograph.TechnicalTest.Tests.Controllers
 {
     public class PersonControllerTest
     {
-        IRepository<Person> _repository;
 
-        public PersonControllerTest()
-        {
-            _repository = A.Fake<IRepository<Person>>();
-        }
-
+        #region GetAll Tests
         [Fact]
-        public async Task PersonController_GetAllPerson_ReturnOKAsync()
+        public async Task PersonController_GetAll_ReturnOKAsync()
         {
             //Arrange
-
-            var controller = new PersonController(_repository);
-
+            var repository = new Mock<IRepository<Person>>();
+            repository.Setup(repository => repository.GetAll()).ReturnsAsync(new List<Person>());
+            var controller = new PersonController(repository.Object);
 
             //Act
             var actionResult = await controller.GetAll();
 
             //Assert
-            actionResult.Should().BeOfType(typeof(OkObjectResult));
+            Assert.IsType<OkObjectResult>(actionResult);
+
         }
 
         [Fact]
-        public async Task PersonController_GetPersonById_ReturnOKAsync()
+        public async Task PersonController_GetAll_ReturnAllPersons()
         {
             //Arrange
-            var controller = new PersonController(_repository);
+            var personList = new List<Person>()
+            {
+                new Person(),
+                new Person(),
+                new Person(),
+                new Person()
+            };
+
+            var repository = new Mock<IRepository<Person>>();
+            repository.Setup(repository => repository.GetAll()).ReturnsAsync(personList);
+            var controller = new PersonController(repository.Object);
+
+            //Act
+            var actionResult = await controller.GetAll() as OkObjectResult;
+
+            //Assert
+            var resultList = actionResult.Value as List<Person>;
+            Assert.Equal(personList.Count, resultList.Count);
+        }
+
+        #endregion
+
+        #region GetById Tests
+        [Fact]
+        public async Task PersonController_GetById_ReturnOkAsync()
+        {
+            //Arrange
+            var repository = new Mock<IRepository<Person>>();
             int testId = 5;
+            repository.Setup(repository => repository.Get(p => p.Id == testId)).ReturnsAsync(new Person() { Id = testId });
+            var controller = new PersonController(repository.Object);
 
             //Act
             var actionResult = await controller.GetById(testId);
 
             //Assert
-            actionResult.Should().BeOfType(typeof (OkObjectResult));
+            Assert.IsType<OkObjectResult>(actionResult);
+
         }
 
         [Fact]
-        public async Task PersonController_Add_ReturnCreatedAtAction()
+        public async Task PersonController_GetById_ReturnBadRequest_When_PersonId_IsDifferent_From_ArgumentId()
         {
             //Arrange
-            var person = A.Fake<Person>();
-            var controller = new PersonController(_repository);
+
+            int fakeId = 7;
+            var repository = new Mock<IRepository<Person>>();
+            repository.Setup(repository => repository.Get(p => p.Id == 5)).ReturnsAsync(new Person() { Id = 5 });
+
+            var controller = new PersonController(repository.Object);
+
+            //Act
+            var actionResult = await controller.GetById(fakeId);
+
+            //Assert
+            Assert.IsType<BadRequestObjectResult>(actionResult);
+        }
+
+        [Fact]
+        public async Task PersonController_GetById_ReturnBadRequest_When_Person_IsNull()
+        {
+            //Arrange
+
+            int fakeId = 9999;
+            var repository = new Mock<IRepository<Person>>();
+            repository.Setup(repository => repository.Get(p => p.Id == 5)).ReturnsAsync(new Person() { Id = 5 });
+
+            var controller = new PersonController(repository.Object);
+
+            //Act
+            var actionResult = await controller.GetById(fakeId);
+
+            //Assert
+            Assert.IsType<BadRequestObjectResult>(actionResult);
+        }
+
+        [Fact]
+        public async Task PersonController_GetById_ReturnPerson_Where_PersonId_EqualTo_ArgumentId()
+        {
+            //Arrange
+            int testId = 5;
+            var expectedPerson = new Person { Id = testId };
+            var repository = new Mock<IRepository<Person>>();
+            repository.Setup(repository => repository.Get(p => p.Id == testId)).ReturnsAsync(new Person() { Id = testId });
+            var controller = new PersonController(repository.Object);
+
+            //Act
+            var actionResult = await controller.GetById(testId) as OkObjectResult;
+
+            //Assert
+            Person resultValue = actionResult.Value as Person;
+            Assert.Equal(expectedPerson.Id, resultValue.Id);
+
+        }
+        #endregion
+
+        #region Add Tests
+        [Fact]
+        public async Task PersonController_Add_ReturnCreatedAtActionResult()
+        {
+            //Arrange
+            var person = new Person();
+            var repository = new Mock<IRepository<Person>>();
+            repository.Setup(repository => repository.Add(person)).ReturnsAsync(person);
+            var controller = new PersonController(repository.Object);
 
             //Act
             var actionResult = await controller.Add(person);
 
             //Assert
-            actionResult.Should().BeOfType(typeof(CreatedAtActionResult));
+            Assert.IsType<CreatedAtActionResult>(actionResult);
+        }
+
+        #endregion
+
+        #region Put Tests
+        [Fact]
+        public async Task PersonController_Update_Return_OkObjectResult()
+        {
+            //Arrange
+            int testId = 5;
+            Person person = new Person() { Id = 5};
+            Person newPerson = new Person() { Id = 5, FirstName = "BIM" , LastName ="BOOM"};
+            var repository = new Mock<IRepository<Person>>();
+            repository.Setup(repository => repository.Get(p => p.Id == testId)).ReturnsAsync(person);
+            repository.Setup(repository => repository.Update(newPerson)).ReturnsAsync(newPerson);
+            var controller = new PersonController(repository.Object);
+
+            //Act
+            var actionResult = await controller.Update(testId, newPerson);
+
+            //Assert
+            Assert.IsType<OkObjectResult>(actionResult);
+
+        }
+        [Fact]
+        public async Task PersonController_Update_Return_NotFoundResult_When_NoPersonFound_WithRouteId()
+        {
+            //Arrange
+            int testId = 1500;
+            Person person  = null;
+            var repository = new Mock<IRepository<Person>>();
+            repository.Setup(repository => repository.Get(p => p.Id == testId)).ReturnsAsync(person);
+            var controller = new PersonController(repository.Object);
+
+            //Act
+            var actionResult = await controller.Update(testId, person);
+
+            //Assert
+            Assert.IsType<NotFoundResult>(actionResult);
+        }
+
+        [Fact]
+        public async Task PersonController_Update_Return_BadRequestResult_When_PersonId_And_ArgumentId_Are_Different()
+        {
+            //Arrange
+            int testId = 123;
+            Person person = new Person() { Id = 5};
+            var repository = new Mock<IRepository<Person>>();
+            repository.Setup(repository => repository.Get(p => p.Id == testId)).ReturnsAsync(person);
+            var controller = new PersonController(repository.Object);
+
+            //Act
+            var actionResult = await controller.Update(testId, person);
+
+            //Assert
+            Assert.IsType<BadRequestResult>(actionResult);
+        }
+
+        [Fact]
+        public async Task PersonController_Update_Return_Person()
+        {
+            //Arrange
+            int testId = 5;
+            Person person = new Person() { Id = 5 };
+            Person newPerson = new Person() { Id = 5, FirstName = "BIM", LastName = "BOOM" };
+            var repository = new Mock<IRepository<Person>>();
+            repository.Setup(repository => repository.Get(p => p.Id == testId)).ReturnsAsync(person);
+            repository.Setup(repository => repository.Update(newPerson)).ReturnsAsync(newPerson);
+            var controller = new PersonController(repository.Object);
+
+            //Act
+            var actionResult = await controller.Update(testId, newPerson) as OkObjectResult;
+
+            //Assert
+            var resultValue = actionResult.Value as Person;
+            Assert.Equal(newPerson, resultValue);
+        }
+        #endregion
+        #region Delete Tests
+        [Fact]
+        public async Task PersonController_Delete_Return_OkObjectResult()
+        {
+            //Arrange
+            int testId = 5;
+            Person person = new Person() { Id = 5 };
+            var repository = new Mock<IRepository<Person>>();
+            repository.Setup(repository => repository.Get(p => p.Id == testId)).ReturnsAsync(person);
+            var controller = new PersonController(repository.Object);
+
+            //Act
+            var actionResult = await controller.Delete(testId);
+
+            //Assert
+            Assert.IsType<OkObjectResult>(actionResult);
 
         }
 
+        [Fact]
+        public async Task PersonController_Delete_Return_BadRequestResult_When_NoPersonFound_From_RouteId()
+        {
+            //Arrange
+            int testId = 5;
+            Person person = new Person() { Id = testId };
+            var repository = new Mock<IRepository<Person>>();
+            repository.Setup(repository => repository.Get(p => p.Id == testId)).ReturnsAsync(person);
+            var controller = new PersonController(repository.Object);
 
+            //Act
+            var actionResult = await controller.Delete(testId);
+
+            //Assert
+            Assert.IsType<OkObjectResult>(actionResult);
+
+        }
+        #endregion
     }
 }
